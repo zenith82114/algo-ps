0d4d48a3a3a1b000188dbff820725d3bd1c0cb4a
zenith82114 <zenith82114@gmail.com>
2026-05-31 01:25:52 +0900

    JUNGOL 15618

--- sources/ICPC/Regionals/Europe/Northwestern Europe Regional Contest (NWERC)/2013/A.cpp
+++ sources/ICPC/Regionals/Europe/Northwestern Europe Regional Contest (NWERC)/2013/A.cpp
@@ -1,28 +1,22 @@
 /*
  * ICPC, Regionals, Europe, Northwestern Europe Regional Contest,
  * NWERC 2013 A. Absurdistan Roads
- * BOJ 9643 - greedy, minimum spanning tree
- * Date: 2025.10.6
+ * JUNGOL 15618
+ *
+ * greedy, minimum spanning tree (O(n^2) for dense graph)
+ * Date: 2026.5.31
  */
 
 #include<bits/stdc++.h>
 using namespace std;
 using i64 = int64_t;
 using edge = tuple<int, int, int>;
+const int MAXN = 2004;
 
-int w[2004][2004];
-bitset<2004> sel;
-vector<pair<int, int> > g[2004];
-priority_queue<edge, vector<edge>, greater<edge> > pq;
-int d[2004];
+int w[MAXN][MAXN], wt[MAXN][MAXN];
+int min_w[MAXN], argmin_w[MAXN];
+bitset<MAXN> sel;
 vector<edge> ans;
-
-void dfs(int p, int i) {
-    for (auto [j, w0] : g[i]) if (j != p) {
-        d[j] = d[i] + w0;
-        dfs(i, j);
-    }
-}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -31,30 +25,33 @@ int main() {
         for (int i = 1; i <= n; ++i)
         for (int j = 1; j <= n; ++j) cin >> w[i][j];
 
-        sel.reset();
-        sel.set(1);
-        for (int i = 1; i <= n; ++i) g[i].clear();
-        for (int j = 2; j <= n; ++j) pq.emplace(w[1][j], 1, j);
+        for (int j = 2; j <= n; ++j) min_w[j] = w[1][j], argmin_w[j] = 1;
+        sel.reset(); sel.set(1);
         ans.clear();
-        while (!pq.empty()) {
-            auto e = pq.top(); pq.pop();
-            auto& [w0, i, j] = e;
-            if (!sel[j]) {
-                sel.set(j);
-                g[i].emplace_back(j, w0);
-                g[j].emplace_back(i, w0);
-                for (int k = 1; k <= n; ++k) if (!sel[k]) pq.emplace(w[j][k], j, k);
-                ans.emplace_back(e);
+
+        for (int t = 1; t < n; ++t) {
+            int w0 = INT32_MAX, j = -1;
+            for (int k = 2; k <= n; ++k) if (!sel[k] && w0 > min_w[k]) {
+                w0 = min_w[k], j = k;
+            }
+
+            int i = argmin_w[j];
+            ans.emplace_back(w0, i, j);
+
+            for (int k = 1; k <= n; ++k) if (sel[k]) {
+                wt[k][j] = wt[j][k] = wt[i][k] + w0;
+            }
+            sel.set(j);
+            for (int k = 2; k <= n; ++k) if (!sel[k] && min_w[k] > w[j][k]) {
+                min_w[k] = w[j][k];
+                argmin_w[k] = j;
             }
         }
 
         edge last(INT32_MAX, 0, 0);
-        for (int i = 1; i <= n; ++i) {
-            d[i] = 0;
-            dfs(0, i);
-            for (int j = 1; j <= n; ++j) if (d[j] > w[i][j]) {
-                last = min(last, edge(w[i][j], i, j));
-            }
+        for (int i = 1; i <= n; ++i)
+        for (int j = i+1; j <= n; ++j) if (wt[i][j] > w[i][j]) {
+            last = min(last, edge(w[i][j], i, j));
         }
         ans.emplace_back(get<0>(last) < INT32_MAX? last : ans[0]);
 
